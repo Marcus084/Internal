ae407ee8cb6492d3af7b5442a6cecdfea9e148dc
Marcus084 <17mmillan@wakatipu.school.nz>
2020-06-15 03:13:31 +1200

    commmit

--- javascript.js
+++ javascript.js
@@ -1,14 +1,1 @@
 console.log("Hello world!");
-
-function Overflow() {
-document.getElementById('7').style.overflowY = 'hidden';
-}
-
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
